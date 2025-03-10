bf200502cc6a06816b45dba9155540cf024cde48
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-03-10 09:52:26 +0000

    Create Project Structure

---
+++ Mobile_Development_CA2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobile_Development_CA2
+//
+//  Created by Student on 10/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobile_Development_CA2/Mobile_Development_CA2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Development_CA2App.swift
+//  Mobile_Development_CA2
+//
+//  Created by Student on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Development_CA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
