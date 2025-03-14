5412dc75ffbd36a1d33a229b101d6d590921d15e
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-03-14 12:43:16 +0000

    Implement navigation between login and register pages

--- Mobile_Development_CA2/Views/LoginView.swift
+++ Mobile_Development_CA2/Views/LoginView.swift
@@ -13,109 +13,112 @@ struct LoginView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack {
-            
-            //Placeholder for Logo
-            Text("Login")
-                .font(.largeTitle)
-                .padding()
-            
-            TextField("Username", text: $username)
-                .font(Font.system(size: 20))
-                .padding(9)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
-                .frame(width: 330, height: 100)
-            
-            SecureField("Password", text: $password)
-                .font(Font.system(size: 20))
-                .padding(9)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
-                .frame(width: 330)
-            
-            HStack {
-                Spacer()
-                Button(action: {
+        NavigationView {
+            VStack {
                 
-                }) {
-                    Text("Forgot Password?")
-                        .foregroundColor(.blue)
-                }
-            }
-            .padding(.horizontal, 20)
-            
-            Button(action: {
-                
-            }) {
+                //Placeholder for Logo
                 Text("Login")
-                    .foregroundColor(.white)
+                    .font(.largeTitle)
                     .padding()
-                    .frame(width: 325, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(8)
-            }
-            .padding(.top, 20)
-            .padding(.bottom, 20)
-
-            HStack {
-                Divider()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                    .background(Color.gray)
                 
-                Text("OR")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                    .padding(.horizontal, 10)
+                TextField("Username", text: $username)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330, height: 100)
                 
-                Divider()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                    .background(Color.gray)
-            }
-
-
-            
-            SignInWithAppleButton(
-                onRequest: { request in
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                },
-                onCompletion: { result in
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                }
-            )
-            .frame(width: 325,height: 50)
-            .padding(.top, 20)
-            .padding(.bottom)
-            
-            VStack{
-                Spacer()
-                Divider()
-                    .frame(height: 1)
-                    .background(Color.gray)
-                    .padding(.vertical, 10)
+                SecureField("Password", text: $password)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330)
                 
                 HStack {
-                    Text("Don't have an Account?")
-                        .foregroundColor(.gray)
-                    
+                    Spacer()
                     Button(action: {
                         
                     }) {
-                        Text("Sign Up")
+                        Text("Forgot Password?")
                             .foregroundColor(.blue)
-                            .fontWeight(.bold)
                     }
                 }
+                .padding(.horizontal, 20)
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 325, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                .padding(.top, 20)
+                .padding(.bottom, 20)
+                
+                HStack {
+                    Divider()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .background(Color.gray)
+                    
+                    Text("OR")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 10)
+                    
+                    Divider()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .background(Color.gray)
+                }
+                
+                
+                
+                SignInWithAppleButton(
+                    onRequest: { request in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    },
+                    onCompletion: { result in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    }
+                )
+                .frame(width: 325,height: 50)
+                .padding(.top, 20)
+                .padding(.bottom)
+                
+                VStack{
+                    Spacer()
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color.gray)
+                        .padding(.vertical, 10)
+                    
+                    HStack {
+                        Text("Don't have an Account?")
+                            .foregroundColor(.gray)
+                        
+                            NavigationLink(destination: RegisterView()) {
+                                Text("Sign Up")
+                                    .foregroundColor(.blue)
+                                    .fontWeight(.bold)
+                            
+                        }
+                    }
+                }
+                .padding(.top, 110)
+                
             }
-            .padding(.top, 110)
-            
+            .frame(alignment: .center)
+            .padding()
+            .padding(.top, 130)
+            .padding(.bottom, 20)
         }
-        .frame(alignment: .center)
-        .padding()
-        .padding(.top, 130)
     }
-    
+        
 }
+    
 
 #Preview {
     LoginView()
--- Mobile_Development_CA2/Views/RegisterView.swift
+++ Mobile_Development_CA2/Views/RegisterView.swift
@@ -13,96 +13,82 @@ struct RegisterView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack {
-            
-            //Placeholder for Logo
-            Text("Register")
-                .font(.largeTitle)
-                .padding()
-            
-            TextField("Username", text: $username)
-                .font(Font.system(size: 20))
-                .padding(9)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
-                .frame(width: 330, height: 100)
-            
-            SecureField("Password", text: $password)
-                .font(Font.system(size: 20))
-                .padding(9)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
-                .frame(width: 330)
-            
-            Button(action: {
+            VStack {
                 
-            }) {
+                //Placeholder for Logo
                 Text("Register")
-                    .foregroundColor(.white)
+                    .font(.largeTitle)
                     .padding()
-                    .frame(width: 325, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(8)
-            }
-            .padding(.top, 20)
-            .padding(.bottom, 20)
-
-            HStack {
-                Divider()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                    .background(Color.gray)
                 
-                Text("OR")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                    .padding(.horizontal, 10)
+                TextField("Full Name", text: $username)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330)
                 
-                Divider()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                    .background(Color.gray)
-            }
-            
-            SignInWithAppleButton(
-                onRequest: { request in
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                },
-                onCompletion: { result in
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                SecureField("Password", text: $password)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330, height: 100)
+                
+                SecureField("Confirm Password", text: $password)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330)
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Register")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 325, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(8)
                 }
-            )
-            .frame(width: 325,height: 50)
-            .padding(.top, 20)
-            .padding(.bottom)
-            
-            VStack{
-                Spacer()
-                Divider()
-                    .frame(height: 1)
-                    .background(Color.gray)
-                    .padding(.vertical, 10)
+                .padding(.top, 20)
+                .padding(.bottom, 20)
                 
                 HStack {
-                    Text("Already have an Account?")
-                        .foregroundColor(.gray)
+                    Divider()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .background(Color.gray)
                     
-                    Button(action: {
-                        
-                    }) {
-                        Text("Log In")
-                            .foregroundColor(.blue)
-                            .fontWeight(.bold)
-                    }
+                    Text("OR")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 10)
+                    
+                    Divider()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .background(Color.gray)
                 }
+                
+                SignInWithAppleButton(
+                    onRequest: { request in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    },
+                    onCompletion: { result in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    }
+                )
+                .frame(width: 325,height: 50)
+                .padding(.top, 20)
+                .padding(.bottom)
+                
+                
             }
-            .padding(.top, 110)
-            
+            .frame(alignment: .center)
+            .padding()
+            .padding(.top, 130)
+            .padding(.bottom, 170)
         }
-        .frame(alignment: .center)
-        .padding()
-        .padding(.top, 130)
     }
-    
-}
 
 #Preview {
     RegisterView()
