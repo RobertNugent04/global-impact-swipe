b354ef1f373cbf34f181682ffd91a50be6466a65
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-03-24 09:39:01 +0000

    Create ForgotPasswordView

---
+++ Mobile_Development_CA2/Views/ForgotPassword.swift
@@ -0,0 +1,72 @@
+//
+//  ForgotPassword.swift
+//  Mobile_Development_CA2
+//
+//  Created by Student on 24/03/2025.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+struct ForgotPasswordView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                VStack {
+                    Image("logo-no-background")
+                        .resizable()
+                        .frame(width: 325, height: 175)
+                        .scaledToFit()
+                    
+                    Text("Global Impact Swipe")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hex: "#333333"))
+                }
+                .padding(.bottom, 35)
+                
+                Text("Please enter your e-mail address")
+                    .font(Font.system(size: 20))
+                    .padding(.top, 30)
+                
+                TextField("E-Mail", text: $username)
+                    .font(Font.system(size: 20))
+                    .padding(9)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.1)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 330, height: 100)
+                
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Submit")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 325, height: 50)
+                        .background(Color(hex: "#4CAF50"))
+                        .cornerRadius(8)
+                }
+                .padding(.top, 10)
+                .padding(.bottom, 20)
+                
+
+
+            }
+            .frame(alignment: .center)
+            .padding()
+
+            .padding(.bottom, 240)
+        }
+    }
+        
+}
+    
+
+#Preview {
+    ForgotPasswordView()
+}
