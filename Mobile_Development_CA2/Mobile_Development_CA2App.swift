046683f1a081d1b786690dc92ec83a16dc2c6dc7
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-04-04 12:20:21 +0100

    Configure Firebase with App

--- Mobile_Development_CA2/Mobile_Development_CA2App.swift
+++ Mobile_Development_CA2/Mobile_Development_CA2App.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
+
+
 
 @main
 struct Mobile_Development_CA2App: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
