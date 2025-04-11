098fce40e91fe8c550a2deb3df10ee68460cbb65
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-04-11 09:58:43 +0100

    Implement an interactive map in MapView

---
+++ Mobile_Development_CA2/Views/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Mobile_Development_CA2
+//
+//  Created by Student on 11/04/2025.
+//
+
+//Initial Map code from apple tutorial: https://developer.apple.com/tutorials/swiftui/handling-user-input
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+
+    var body: some View {
+        Map(position: .constant(.region(region)))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+}
