6eb0caff8fe818f258a0091e1349c0f2a8b724a4
PatrickOrjieh <orjiehpatrick@gmail.com>
2025-04-16 21:35:12 +0100

    Add main title to the bottom overlay

--- Mobile_Development_CA2/Views/IntroScreenView.swift
+++ Mobile_Development_CA2/Views/IntroScreenView.swift
@@ -25,7 +25,30 @@ struct IntroScreenView: View {
                         .cornerRadius(60, corners: [.topLeft, .topRight])
                         .shadow(color: .black.opacity(0.05), radius: 10, x: 0, y: -5)
                     
-                    Spacer()
+                    VStack{
+                        VStack(spacing: 0) {
+                            Text("Swipe to make an")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            
+                            Text("impact")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color(hex: "#4CAF50"))
+                            
+                            Text("One project at a time")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 30)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 50)
                 }
                 .offset(y: -60)
                 .frame(height: UIScreen.main.bounds.height * 0.40)
