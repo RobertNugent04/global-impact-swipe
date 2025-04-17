e48567d26f4730878b66f737cdc09124d8a94d28
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-04-17 10:54:06 +0100

    Add AutocompleteViewModel to handle location suggestions

---
+++ Mobile_Development_CA2/ViewModels/AutocompleteViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  AutocompleteViewModel.swift
+//  Mobile_Development_CA2
+//
+//  Created by Student on 17/04/2025.
+//
+
+//Some of the following code is taken from:
+//https://hackernoon.com/address-autocompletion-using-swiftui-and-mapkit?utm_source=chatgpt.com
+//https://www.youtube.com/watch?v=cOD1l2lv2Jw
+
+
+import Foundation
+import MapKit
+import Combine
+
+class AutocompleteViewModel: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
+    
+    @Published var searchQuery = ""
+
+    @Published var suggestions: [MKLocalSearchCompletion] = []
+
+    private var completer: MKLocalSearchCompleter
+
+    override init() {
+        self.completer = MKLocalSearchCompleter()
+        super.init()
+
+        // Set this ViewModel as the delegate to receive updates.
+        completer.delegate = self
+
+        // Specify the type of results we want (addresses).
+        completer.resultTypes = .address
+    }
+
+    // Update the autocomplete query whenever the user types in the search field.
+    func updateSearch(query: String) {
+        completer.queryFragment = query
+    }
+
+    // Method called when new autocomplete results are available.
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        DispatchQueue.main.async {
+            self.suggestions = completer.results
+        }
+    }
+
+    // Method called if the search completer fails to fetch results.
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Error fetching suggestions: \(error.localizedDescription)")
+    }
+}
