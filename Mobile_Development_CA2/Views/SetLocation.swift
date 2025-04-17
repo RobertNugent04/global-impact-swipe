5aaac0e38ab42809c5c2c4567de2412280218bf7
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2025-04-17 10:18:41 +0100

    Add location permission check in SetLocationView

--- Mobile_Development_CA2/Views/SetLocation.swift
+++ Mobile_Development_CA2/Views/SetLocation.swift
@@ -10,26 +10,27 @@ import MapKit
 
 struct SetLocationView: View {
     @State var coordinate = CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868)
-    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+
     @State private var searchText = ""
+    @StateObject private var locationManager = LocationManager()
 
     var body: some View {
         ZStack {
-            
             VStack(spacing: 0) {
                 NavbarView()
-                
                 SearchBarView(text: $searchText)
-                
                 MapView(coordinate: coordinate)
                     .edgesIgnoringSafeArea(.bottom)
             }
 
-            // Set Location Button
             VStack {
                 Spacer()
                 Button(action: {
-                    //Set the location of the user
+                    // Set the location of the user
                 }) {
                     Text("Set Location")
                         .fontWeight(.bold)
@@ -42,6 +43,9 @@ struct SetLocationView: View {
                 }
                 .padding(.bottom, 16)
             }
+        }
+        .onAppear {
+            locationManager.checkLocationAuthorization()
         }
     }
 }
