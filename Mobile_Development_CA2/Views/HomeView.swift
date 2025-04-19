c86e630c5c85af5f53ac3219ce5dfad549c2a036
PatrickOrjieh <orjiehpatrick@gmail.com>
2025-04-20 00:30:21 +0100

    Add set location button with icon and styling

--- Mobile_Development_CA2/Views/HomeView.swift
+++ Mobile_Development_CA2/Views/HomeView.swift
@@ -9,6 +9,40 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            HStack {
+                Button(action: {
+                    // i will add the action here
+                }) {
+                    HStack(spacing: 10) {
+                        Image(systemName: "location.fill")
+                            .foregroundColor(Color(hex: "#4CAF50"))
+                            .padding(8)
+                            .background(Circle().fill(Color.white))
+
+                        Text("Set Location")
+                            .foregroundColor(.white)
+                            .fontWeight(.medium)
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 8)
+                    .background(Color.black)
+                    .cornerRadius(30)
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 2)
+            .padding(.horizontal, 20)
+
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
+}
+
+
+
+#Preview {
+    HomeView()
 }
